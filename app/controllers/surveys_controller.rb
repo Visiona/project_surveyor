580a71208a1dd7752dab3c2383cb8084825471c8
Visiona <darbis@gmail.com>
2017-05-26 21:51:36 +0100

    Create a Survey story completed

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -5,4 +5,24 @@ class SurveysController < ApplicationController
     @surveys = Survey.all
   end
 
+  def new
+    @survey = Survey.new
+  end
+
+  def create
+    @survey = Survey.new(whitelisted_user_params)
+    if @survey.save
+      flash[:success] = "New Survey initiated"
+      redirect_to root_path #possibly /survey/questions ??
+    else
+      flash.now[:danger] = "New Survey could not be initiated."
+      render 'new'
+    end
+  end
+
+  private
+  def whitelisted_user_params
+    params.require(:survey).permit(:title, :description)
+  end
+
 end
