79160fb22008d32772feb33c9bb45214d7e21518
Visiona <darbis@gmail.com>
2017-06-16 22:01:07 +0100

    Fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,26 @@ class UsersController < ApplicationController
   def create
     @survey = Survey.find(params[:survey_id])
     @user = User.new(whitelisted_answers_params)
+    n = params[:user][:multi_answers_attributes].size
+    n.times do |i|
+      choices = params[:user][:multi_answers_attributes]["#{i}"][:choice_id]
+      param_multi_q_id = params[:user][:multi_answers_attributes]["#{i}"][:multi_q_id]
+      if choices.nil? && @survey.multi_qs.where(:id => param_multi_q_id.to_i)[0].required
+        @survey.multi_qs.where(:id => param_multi_q_id.to_i)[0].errors.add(:choice_id, "This Question is required")
+      end
+      if choices.is_a?(Array) || choices.nil?
+        for_deletion = []
+        @user.multi_answers.each do |ma|
+          if ma.multi_q_id == param_multi_q_id.to_i
+            for_deletion << ma
+          end
+        end
+        for_deletion.each {|ma| ma.destroy}
+        if choices.is_a? Array 
+          choices.each {|ch| @user.multi_answers.build(:multi_q_id => param_multi_q_id.to_i, :choice_id => ch) }
+        end
+      end
+    end
 
     if @user.save
       flash[:success] = "Thank you for submitting your survey."
--- app/models/multi_answer.rb
+++ app/models/multi_answer.rb
@@ -2,4 +2,6 @@ class MultiAnswer < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :multi_q
   belongs_to :choice, optional: true
+
+  #validates add
 end
